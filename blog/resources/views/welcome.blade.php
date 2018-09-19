b94368c702346a9df533b19e93e1f16e4e755278
TheHanEmperor <hs267@mail.missouri.edu>
2018-09-18 22:56:12 -0500

    first scratch

--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -78,7 +78,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hello world
                 </div>
 
                 <div class="links">
