c37e4f5875f95e2730fd7725664534820b1948ad
TheHanEmperor <hs267@mail.missouri.edu>
2018-09-25 17:13:56 -0500

    get rid of id

--- sql/Team Standard Batting.sql
+++ sql/Team Standard Batting.sql
@@ -1,2 +1,38 @@
-DROP TABLE IF EXISTS Team;
-CREATE TABLE `games` (
+DROP TABLE IF EXISTS Team_Bating;
+CREATE TABLE `Team_Bating` (
+  
+  `Team` varchar(8) ,
+  `batNum` int,
+  `BatAge` int,
+  `R/G` int,
+  `G` int,
+  `PA` int,
+  `AB` int,
+  `R` int,
+  `H` int,
+  `2B` int,
+  `3B` int,
+  `HR` int,
+  `RBI` int,
+  `SB` int,
+    `CS` int,
+  `BB` int,
+  `SO` int,
+  `BA` int,
+  `OBP` int,
+  `SLG` int,
+  `OPS` int,
+  `OPS+` int,
+  `TB` int,
+  `GDP` int,
+  `HBP` int,
+  `SH` int,
+  `SF` int,
+  `IBB` int,
+  `LOB` int,
+  PRIMARY KEY(Team)
+
+
+
+
+);
