6714b7807fc916984c2300983f9fd7cb91ccbcca
David Heritage <dheritage21@gmail.com>
2018-10-11 17:18:45 -0500

    Add files via upload

--- source.py
+++ source.py
@@ -1,67 +1,44 @@
 import csv
 import mysql.connector
 from mysql.connector import errorcode
-from sklearn.naive_bayes import GaussianNB
+import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import time
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB, BernoulliNB, MultinomialNB
 
 
 
 
 def main():
-    try:
-        cnx = mysql.connector.connect(user='capstoneAdmin', password='12345678',
-                                  host='ec2-52-6-86-207.compute-1.amazonaws.com',
-                                  database='bayesball')
-
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Something is wrong with your user name or password")
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Database does not exist")
-        else:
-            print(err)
-        
-    m = cnx.cursor()
-
-    yearBreakdown_dataset = "SELECT id, Rdiff, SRS, R_G, R, RBI, OBP, OPSP, RA, ERA, SV, HA, ER, ERAP, WHIP FROM yearBreakdown;"
-    m.execute(yearBreakdown_dataset)
-    result = m.fetchall()
+    data = pd.read_csv("yearBreakdown.csv")
+    # Convert categorical variable to numeric
+    #print(data.iloc[:,3])
+    #data=data.iloc[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15].dropna(axis=1, how='any')
+    #data=data.ix[1,2,3].dropna(axis=0, how='any')
+    # Split dataset in training and test datasets
+    X_train, X_test = train_test_split(data, test_size=0.33, random_state=int(time.time()))
+    # Instantiate the classifier
+    gnb = GaussianNB()
+    used_features =[
+          "Rdiff"
+          ]
     
-    fp = open("yearBreakdown.csv","w")
-    myFile = csv.writer(fp)
-    myFile.writerows(result)
+    gnb.fit(
+    X_train[used_features].values,
+    X_train[data.iloc[15]]
+    )
     
-    dataset = []
-    with open("yearBreakdown.csv") as csvfile:
-        reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC) # change contents to floats
-        for row in reader: # each row is a list
-            dataset.append(row) 
-            
-      
-    yearBreakdown_dataset = "SELECT Outcome FROM yearBreakdown;"
-    m.execute(yearBreakdown_dataset)
-    result2 = m.fetchall()
+    y_pred = gnb.predict(X_test[used_features])
     
-    fp2 = open("yearBreakdown2.csv","w")
-    myFile = csv.writer(fp2)
-    myFile.writerows(result2)
     
-    outcomeDataset = []
-    with open("yearBreakdown2.csv") as csvfile:
-        reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC) # change contents to floats
-        for row in reader: # each row is a list
-            outcomeDataset.append((row))    
-        
-    
-    #bayseball = result
-    
-    #bayseball = result2
-    #bayseball. = result3
-    #gnb = GaussianNB()
-    #y_pred = gnb.fit(result2, result3).predict(result2)
-    #print("%d", y_pred)
-    #print("Number of mislabeled points out of a total %d points : %d"
-    #  % (result2,(result3 != y_pred).sum()))
-    
+    # Print results
+    print("Number of mislabeled points out of a total {} points : {}, performance {:05.2f}%"
+          .format(
+              X_test.shape[0],
+              (X_test[data.iloc[15]] != y_pred).sum(),
+              100*(1-(X_test[data.iloc[15]] != y_pred).sum()/X_test.shape[0])
+    ))
     
 main()
