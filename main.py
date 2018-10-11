91c8103a8a10b27cf6cd671bd8188530e8f6288c
David Heritage <dheritage21@gmail.com>
2018-10-11 14:06:07 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,54 +1,141 @@
-import sys
 import random
 import math
 import csv
-from beautifultable import BeautifulTable
 import mysql.connector
 from mysql.connector import errorcode
 
-#linear regression algorithm
 
-#Connect to db
-try:
-    cnx = mysql.connector.connect(user='capstoneAdmin', password='12345678',
+def loadCSV(filename):
+    lines = csv.reader(open(filename, "r"))
+    dataset = list(lines)
+    for i in range(len(dataset)):
+        dataset[i] = [float(x) for x in dataset[i]]        
+    return dataset
+
+
+def splitDataset(dataset, splitRatio):
+        trainSize = int(len(dataset) * splitRatio)
+        trainSet = []
+        copy = list(dataset)
+        while len(trainSet) < trainSize:
+            index = random.randrange(len(copy))
+            trainSet.append(copy.pop(index))
+        return [trainSet, copy]            
+
+
+def seperateByClass(dataset):
+    separated = {}
+    for i in range(len(dataset)):
+        vector = dataset[i]
+        if(vector[-1] not in separated):
+            separated[vector[-1]] = []
+            separated[vector[-1]].append(vector)
+    return separated
+
+
+def mean(numbers):
+    return sum(numbers)/float(len(numbers))
+
+
+def stdev(numbers):
+    avg = mean(numbers)
+    variance = sum([pow(x-avg,2) for x in numbers])/float(len(numbers)-1)
+    return math.sqrt(variance)
+
+
+def summarize(dataset):
+    summaries = [(mean(attribute), stdev(attribute)) for attribute in zip(*dataset)]
+    del summaries[-1]
+    return summaries
+
+
+def summarizeByClass(dataset):
+    separated = seperateByClass(dataset)
+    summaries = {}
+    for classValue, instances in separated.items():
+        summaries[classValue] = summarize(instances)
+    return summaries
+
+
+def caculateProbability(x, mean, stdev):
+    exponent = math.exp(-(math.pow(x-mean,2)/(2*math.pow(stdev,2))))
+    return (1/(math.sqrt(2*math.pi)*stdev))*exponent
+
+
+def calculateClassProbabilities(summaries, inputVector):
+    probabilities = {}
+    for classValue, classSummaries in summaries.items():
+        probabilities[classValue] = 1
+        for i in range(len(classSummaries)):
+            mean, stdev = classSummaries[i]
+            x = inputVector[i]
+            probabilities[classValue] *= caculateProbability(x, mean, stdev)
+        return probabilities
+        
+    
+def predict(summaries, inputVector):
+    probabilities = calculateClassProbabilities(summaries, inputVector)
+    bestLabel, bestProb = None, -1
+    for classValue, probability in probabilities.items():
+        if bestLabel is None or probability > bestProb:
+            bestProb = probability
+            bestLabel = classValue
+        return bestLabel
+    
+        
+def getPredictions(summaries, testSet):
+    predictions = []
+    for i in range(len(testSet)):
+        result = predict(summaries, testSet[i])
+        predictions.append(result)
+    return predictions
+
+
+def getAccuracy(testSet, prediction):
+    correct = 0
+    for x in range(len(testSet)):
+        if testSet[x][-1] == prediction[x]:
+            correct += 1
+    return(correct/float(len(testSet)))*100
+
+
+def main():
+    try:
+        cnx = mysql.connector.connect(user='capstoneAdmin', password='12345678',
                                   host='ec2-52-6-86-207.compute-1.amazonaws.com',
                                   database='bayesball')
 
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with your user name or password")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database does not exist")
-    else:
-        print(err)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+        
+    m = cnx.cursor()
 
+    yearBreakdown_dataset = "SELECT id, Rdiff, SRS, R_G, R, RBI, OBP, OPSP, RA, ERA, SV, HA, ER, ERAP, WHIP, Outcome FROM yearBreakdown;"
+    m.execute(yearBreakdown_dataset)
+    result = m.fetchall()
     
-m = cnx.cursor()
-#set first set of data to variable
-bating_dataset = "SELECT * FROM Team_Bating;"
-m.execute(bating_dataset)
-result = m.fetchall()
-#Write data to csv file
-fp = open("BatingData.csv","w")
-myFile = csv.writer(fp)
-myFile.writerows(result)
-fp.close()
-
-games_dataset = "SELECT * FROM games;"
-
-m.execute(games_dataset)
-games_result = m.fetchall()
-
-
-fp2 = open("GamesData.csv","w")
-myFile = csv.writer(fp2)
-myFile.writerows(games_result)
-fp2.close()
-
-
-print("Success")
+    fp = open("yearBreakdown.csv","w")
+    myFile = csv.writer(fp)
+    myFile.writerows(result)
+    
+    filename = 'yearBreakdown.csv'
+    splitRatio = 0.67
+    dataset = loadCSV(filename)
+    print(len(filename))
+    trainingSet, testSet = splitDataset(dataset, splitRatio)
+    print('Split {0} rows into train = {1} and test = {2} rows'.format(len(dataset),len(trainingSet),len(testSet)))
+    #prepare model
+    summaries = summarizeByClass(trainingSet)
+    #test model
+    predictions = getPredictions(summaries, testSet)
+    accuracy = getAccuracy(testSet, predictions)
+    print('Accuracy: {0}%'.format(accuracy))
     
     
     
-
-cnx.close()
+main()
