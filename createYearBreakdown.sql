b9cf50e9fa5c9f30f18012257d99cfe841536a04
wjohnke <wjohnke@gmail.com>
2018-10-11 00:53:35 -0500

    Fix issue w/ misplaced home/visitor rows

--- createYearBreakdown.sql
+++ createYearBreakdown.sql
@@ -21,18 +21,32 @@ CREATE TABLE yearBreakdown(
 	ER decimal(8,3),
 	ERAP decimal(8,3),
 	WHIP decimal(8,3),
+	Outcome boolean,
 	PRIMARY KEY(id)
 );
 
+/*
 INSERT INTO yearBreakdown
 	SELECT NULL, Team, games.game_date, games.game_number, games.visitor_score, games.home_score, Rdiff, SRS, R/G, R, RBI, OBP, OPSP, RA, ERA, SV, Team_Bating.HA, ER, ERAP, WHIP
 	from Team_Bating LEFT JOIN games 
 	on (Team_Bating.Team = games.home or Team_Bating.Team = games.visitor);
 	
 ALTER TABLE yearBreakdown ADD Outcome boolean;
-/*Home Team ID: Even, Away Team ID: Odd*/
-UPDATE yearBreakdown SET Outcome = IF(id%2=1, IF(visitor_score>home_score, 1,0),IF(visitor_score<home_score,1,0) );
 
+UPDATE yearBreakdown SET Outcome = IF(id%2=0, IF(visitor_score>home_score, 1,0),IF(visitor_score<home_score,1,0) );	
+*/
 
-
-
+--Insert Home team statistics
+INSERT INTO yearBreakdown
+	SELECT NULL, 
+	Team, games.game_date, games.game_number, games.visitor_score, games.home_score, Rdiff, SRS, R/G, R, RBI, OBP, OPSP, RA, ERA, SV, Team_Bating.HA, ER, ERAP, WHIP,
+	(IF(home_score>visitor_score,1,0))
+	from Team_Bating LEFT JOIN games 
+	on (Team_Bating.Team = games.home);
+--Insert Visitor team statistics
+INSERT INTO yearBreakdown
+	SELECT NULL, 
+	Team, games.game_date, games.game_number, games.visitor_score, games.home_score, Rdiff, SRS, R/G, R, RBI, OBP, OPSP, RA, ERA, SV, Team_Bating.HA, ER, ERAP, WHIP,
+	(IF(visitor_score>home_score,1,0))
+	from Team_Bating LEFT JOIN games 
+	on (Team_Bating.Team = games.visitor);	
