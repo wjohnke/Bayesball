7a40bfac5494485e5f07fa920b47cd3d01fbb32d
TheHanEmperor <hs267@mail.missouri.edu>
2018-10-16 22:07:34 -0500

    finished game & date

--- blog/resources/views/pages/gameDetails.blade.php
+++ blog/resources/views/pages/gameDetails.blade.php
@@ -3,14 +3,34 @@
 <style>
     #container{
 
-        text-align: center;
-        display:flex;
+
+        width :100%;
+        text-align:center;
+
     }
-    #bloc1, #bloc2
-    {
+    #left
+    {   float:left;
+        margin-left: 100px;
+        width:100px;
+        height: 128px;
 
 
     }
+    #center{
+        display: inline-block;
+        margin:0 auto;
+        width:400px;
+        height: 128px;
+    }
+    #right{
+        margin-right: 100px;
+        float:right;
+        width:100px;
+        height: 128px;
+
+    }
+
+
     p.title{
         color:black;
     }
@@ -19,34 +39,84 @@
     <div class="well" >
         <h3 align="left">{{$games->game_date}} {{$games->day}}</h3>
         <div id="container">
-        <div id="bloc1">
+        <div id="left">
             <img src="{{URL::asset("images/teamLogos/{$games->visitor}.png")}}" height="128" alt="" />
-
+            <div > <h2>{{$games->visitor}}</h2></div>
         </div>
-        <div id="bloc2">
-            <p class="title" style="font-size:50px;" align="center">
+            <div id="center">
+                <p class="title" style="font-size:50px;" align="center">
 
 
-                {{$games->visitor_score}}   ---       {{$games->home_score}}
+                    {{$games->visitor_score}} - {{$games->home_score}}
 
-            </p>
-        </div>
+                </p>
+            </div>
+        <div id="right">
+                <img src="{{URL::asset("images/teamLogos/{$games->home}.png")}}" height="128"  alt="" />
+            <div>
+              <h2>{{$games->home}}</h2>
+            </div>
 
-        <div id="bloc3">
-            <img src="{{URL::asset("images/teamLogos/{$games->home}.png")}}" height="128"  alt="" />
 
-        </div>
+        </div >
+
+
+
+
         </div>
 
 
 
 
         <p style="font-size: 45px;" align="center">
-            Visitor Score: {{$games->visitor_score}} vs Home Score: {{$games->home_score}}
 
 
         </p>
 
+    </div>
+    <div class="well">
+        <div class="table">
+            <table width="100%">
+                <tr>
+                    <th>Team</th>
+                    <td margin-right="0" width="5%" align="right">ab</td>
+                    <td margin-right="0" width="5%" align="right">h</td>
+                    <td margin-right="0" width="5%" align="right">2b</td>
+                    <td margin-right="0" width="5%" align="right">3b</td>
+                    <td margin-right="0" width="5%" align="right">hr</td>
+                    <td margin-right="0" width="5%" align="right">sh</td>
+
+
+                </tr>
+                <tr>
+                    <th>{{$games->visitor}}</th>
+
+
+                    <td margin-right="0" align="right">{{$games->visitor_ab}}</td>
+                    <td margin-right="0" align="right">{{$games->visitor_h}}</td>
+                    <td margin-right="0" align="right">{{$games->visitor_2b}}</td>
+                    <td margin-right="0" align="right">{{$games->visitor_3b}}</td>
+                    <td margin-right="0" align="right">{{$games->visitor_hr}}</td>
+                    <td margin-right="0" align="right">{{$games->visitor_sh}}</td>
+
+
+
+
+                </tr>
+                <tr>
+                    <th>{{$games->home}}</th>
+                    <td margin-right="0" align="right">{{$games->home_ab}}</td>
+                    <td margin-right="0" align="right">{{$games->home_h}}</td>
+                    <td margin-right="0" align="right">{{$games->home_2b}}</td>
+                    <td margin-right="0" align="right">{{$games->home_3b}}</td>
+                    <td margin-right="0" align="right">{{$games->home_hr}}</td>
+                    <td margin-right="0" align="right">{{$games->home_sh}}</td>
+
+                </tr>
+
+
+            </table>
+        </div>
     </div>
 @endif
 
